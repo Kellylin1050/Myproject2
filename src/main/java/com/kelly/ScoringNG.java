c3a238b72fcdf0da79cf21d06c82e0bdc341d708
kelly <kellylin1050@gmail.com>
2022-03-04 18:44:33 +0800

    test 4nd commit

--- src/main/java/com/kelly/ScoringNG.java
+++ src/main/java/com/kelly/ScoringNG.java
@@ -5,7 +5,7 @@ public class ScoringNG {
         // 5 students each
         int[] englishs = {88,90,75,45,60};
         int[] maths = {90,100,50,77,62};
-        String[]  name = {"Tank","Selina" ,"William" ,"jane","julia" };
+        String[]  name = {"Tank","Selina" ,"William" ,"jack","julia" };
         for (int i = 0; i < 5; i++) {
             if (i!= 1){
                 System.out.println(name[i] + "\t" + englishs[i] + "\t" + maths[i] + "\t" + (englishs[i] + maths[i]) / 2);
